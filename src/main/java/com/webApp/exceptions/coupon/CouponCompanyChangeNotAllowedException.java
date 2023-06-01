251ac6ff00dc2e9031f9a3e6b57d39cc9f76d668
BillyButcherr <114831035+BillyButcherr@users.noreply.github.com>
2023-06-01 04:49:34 +0300

    Delete src/main directory

--- src/main/java/com/webApp/exceptions/coupon/CouponCompanyChangeNotAllowedException.java
+++
@@ -1,9 +0,0 @@
-package com.webApp.exceptions.coupon;
-
-import com.webApp.exceptions.CustomException;
-
-public class CouponCompanyChangeNotAllowedException extends CustomException {
-    public CouponCompanyChangeNotAllowedException() {
-        super("Coupons are not allowed to change their company id");
-    }
-}
--- src/main/java/com/webApp/exceptions/customer/EmptyCustomerIDException.java
+++
@@ -1,9 +0,0 @@
-package com.webApp.exceptions.customer;
-
-import com.webApp.exceptions.CustomException;
-
-public class EmptyCustomerIDException extends CustomException {
-    public EmptyCustomerIDException() {
-        super("Empty ID field inside customer");
-    }
-}
